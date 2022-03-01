444cf5ddfd5fde8540196aa19b6db66e77e98d2d
prahlad147 <77727870+prahlad147@users.noreply.github.com>
2022-03-01 11:22:41 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyCJoEY7ssFx3IfVIkbJ41l8NMbvOwKkBcM",
+
+    authDomain: "firstproject-51ca7.firebaseapp.com",
+
+    databaseURL: "https://firstproject-51ca7-default-rtdb.firebaseio.com",
+
+    projectId: "firstproject-51ca7",
+
+    storageBucket: "firstproject-51ca7.appspot.com",
+
+    messagingSenderId: "785395969612",
+
+    appId: "1:785395969612:web:ae4826e033994b8a978ee9"
+
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
